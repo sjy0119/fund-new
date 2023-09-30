aa9bf590610eb6dec69c8b139edfbb83bb2f9c7b
WKM2071408123 <146437211+WKM2071408123@users.noreply.github.com>
2023-09-30 14:03:56 +0800

    Update 1_Plotting_Demo.py

--- pages/1_Plotting_Demo.py
+++ pages/1_Plotting_Demo.py
@@ -14,7 +14,7 @@
 
 import time
 import numpy as np
-import plotly as py
+import plotly.graph_objects as go
 import tushare as ts
 import streamlit as st
 from streamlit.hello.utils import show_code
