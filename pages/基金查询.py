b604c36c139331bb01e028106eed852119d028dc
WKM2071408123 <146437211+WKM2071408123@users.noreply.github.com>
2023-10-20 21:42:30 +0800

    Update 基金查询.py

--- pages/基金查询.py
+++ pages/基金查询.py
@@ -1,9 +1,11 @@
-import requests
 import pandas as pd
 import datetime
 from akshare.utils import demjson
-from joblib import Parallel, delayed
+import asyncio
+import aiohttp
 import streamlit as st
+
+
 
 pd.set_option('display.unicode.ambiguous_as_wide', True)
 pd.set_option('display.unicode.east_asian_width', True)
@@ -17,6 +19,9 @@ st.markdown(multi)
 
 name_str=st.text_input('请输入基金类别名称')
 
+current_date = datetime.datetime.now().date().isoformat()
+last_date = str(int(current_date[:4]) - 1) + current_date[4:]
+
 type_map = {
         "股票型": ["gp", "6yzf"],
         "混合型": ["hh", "6yzf"],
@@ -28,112 +33,116 @@ type_map = {
 
 type_list=["gp","hh","zq","zs","qdii","lof","fof"]
 
-def get_fund_rank_data(ip):
-    current_date = datetime.datetime.now().date().isoformat()
-    last_date = str(int(current_date[:4]) - 1) + current_date[4:]
-    url = "http://fund.eastmoney.com/data/rankhandler.aspx"
-
-    params = {
-        "op": "ph",
-        "dt": "kf",
-        "ft": ip,
-        "rs": "",
-        "gs": "0",
-        "sc": "6yzf",
-        "st": "desc",
-        "sd": last_date,
-        "ed": current_date,
-        "qdii": "",
-        "tabSubtype": ",,,,,",
-        "pi": "1",
-        "pn": "20000",
-        "dx": "1",
-        "v": "0.1591891419018292",
-    }
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
-        "Referer": "http://fund.eastmoney.com/fundguzhi.html",
-    }
-    r = requests.get(url, params=params, headers=headers)
-    text_data = r.text
-    json_data = demjson.decode(text_data[text_data.find("{") : -1])
-    temp_df = pd.DataFrame(json_data["datas"])
-    temp_df = temp_df.iloc[:, 0].str.split(",",expand=True)
-    temp_df.reset_index(inplace=True)
-    temp_df["index"] = list(range(1, len(temp_df) + 1))
-    temp_df.columns = [
-        "序号",
-        "基金代码",
-        "基金简称",
-        "_",
-        "日期",
-        "单位净值",
-        "累计净值",
-        "日增长率",
-        "近1周",
-        "近1月",
-        "近3月",
-        "近6月",
-        "近1年",
-        "近2年",
-        "近3年",
-        "今年来",
-        "成立来",
-        "_",
-        "_",
-        "自定义",
-        "_",
-        "手续费",
-        "_",
-        "_",
-        "_",
-        "_",
-    ]
-    temp_df = temp_df[
-        [
-            "序号",
-            "基金代码",
-            "基金简称",
-            "日期",
-            "单位净值",
-            "累计净值",
-            "日增长率",
-            "近1周",
-            "近1月",
-            "近3月",
-            "近6月",
-            "近1年",
-            "近2年",
-            "近3年",
-            "今年来",
-            "成立来",
-            "手续费",
-        ]
-    ]
-    temp_df=temp_df.loc[temp_df['近3年']!='']
-    temp_df=temp_df.apply(pd.to_numeric,errors='ignore')
-    temp_df['基金代码']=temp_df['基金代码'].apply(lambda x:"0000"+str(x))
-    temp_df['基金代码']=temp_df['基金代码'].apply(lambda x: x[-6:])
-    return temp_df
+params_list=[{
+    "op": "ph",
+    "dt": "kf",
+    "ft": ip,
+    "rs": "",
+    "gs": "0",
+    "sc": "6yzf",
+    "st": "desc",
+    "sd": last_date,
+    "ed": current_date,
+    "qdii": "",
+    "tabSubtype": ",,,,,",
+    "pi": "1",
+    "pn": "20000",
+    "dx": "1",
+    "v": "0.1591891419018292",
+} for ip in type_list]
 
 @st.cache_data
 def get_data():
-    df=Parallel(n_jobs=-1)(delayed(get_fund_rank_data)(i) for i in type_list)
-    return df
+    data={}
+    async def async_get_url(params,i):
+        url="http://fund.eastmoney.com/data/rankhandler.aspx"
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
+            "Referer": "http://fund.eastmoney.com/fundguzhi.html",
+        }
+        async with aiohttp.ClientSession() as session:  # 解释1
+            async with session.get(url, params=params,headers=headers) as r:
+                text_data = await r.text()
+                json_data = demjson.decode(text_data[text_data.find("{") : -1])
+                temp_df = pd.DataFrame(json_data["datas"])
+                temp_df = temp_df.iloc[:, 0].str.split(",",expand=True)
+                temp_df.reset_index(inplace=True)
+                temp_df["index"] = list(range(1, len(temp_df) + 1))
+                temp_df.columns = [
+                    "序号",
+                    "基金代码",
+                    "基金简称",
+                    "_",
+                    "日期",
+                    "单位净值",
+                    "累计净值",
+                    "日增长率",
+                    "近1周",
+                    "近1月",
+                    "近3月",
+                    "近6月",
+                    "近1年",
+                    "近2年",
+                    "近3年",
+                    "今年来",
+                    "成立来",
+                    "_",
+                    "_",
+                    "自定义",
+                    "_",
+                    "手续费",
+                    "_",
+                    "_",
+                    "_",
+                    "_",
+                ]
+                temp_df = temp_df[
+                    [
+                        "序号",
+                        "基金代码",
+                        "基金简称",
+                        "日期",
+                        "单位净值",
+                        "累计净值",
+                        "日增长率",
+                        "近1周",
+                        "近1月",
+                        "近3月",
+                        "近6月",
+                        "近1年",
+                        "近2年",
+                        "近3年",
+                        "今年来",
+                        "成立来",
+                        "手续费",
+                    ]
+                ]
+                temp_df=temp_df.loc[temp_df['近3年']!='']
+                temp_df=temp_df.apply(pd.to_numeric,errors='ignore')
+                temp_df['基金代码']=temp_df['基金代码'].apply(lambda x:"0000"+str(x))
+                temp_df['基金代码']=temp_df['基金代码'].apply(lambda x: x[-6:])
+                data[i]=temp_df
+                #print('获取成功')
+    #nest_asyncio.apply()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    tasks = [async_get_url(params,i) for params,i in zip(params_list,type_list)]
+    loop.run_until_complete(asyncio.wait(tasks))
+    return data
 
 df=get_data()
 
 if name_str=='股票型':
-    st.dataframe(df[0])
+    st.dataframe(df['gp'])
 elif name_str=='混合型':
-    st.dataframe(df[1])
+    st.dataframe(df['hh'])
 elif name_str=='债券型':
-    st.dataframe(df[2])
+    st.dataframe(df['zq'])
 elif name_str=='指数型':
-    st.dataframe(df[3])
+    st.dataframe(df['zs'])
 elif name_str=='QDII':
-    st.dataframe(df[4])
+    st.dataframe(df['qdii'])
 elif name_str=='LOF':
-    st.dataframe(df[5])
+    st.dataframe(df['lof'])
 elif name_str=='FOF':
-    st.dataframe(df[6])
+    st.dataframe(df['fof'])
